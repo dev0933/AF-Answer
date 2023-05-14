24c91d42b0666e8a37daf51095b8064aa94b34ff
Azozz ALFiras <azozzalfiras@gmail.com>
2023-05-14 23:54:14 +0300

    Initial Commit

---
+++ AF Answer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AF Answer
+//
+//  Created by Azozz ALFiras on 14/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
